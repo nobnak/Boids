df1bc06847a89333db115c02d6a96020c7717234
nnakata <nakata@team-lab.com>
2013-10-28 10:37:12 +0900

    kyoukai settei

--- Assets/Wander/WanderRigidbody.cs
+++ Assets/Wander/WanderRigidbody.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 public class WanderRigidbody : MonoBehaviour {
-	public float radius;
-	public float distance;
-	public float power;
-	public float noise;
+	public float wanderRadius;
+	public float wanderDistance;
+	public float wanderWeight;
+	public float wanderNoise;
+	
+	public float wallWeight;
 	
 	public float maxSpeed;
 	public float minSpeed;
+	
+	public Collider watertank;
 	
 	private Vector2 _targetLocal;
 
@@ -22,22 +26,37 @@ public class WanderRigidbody : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		var dt = Time.fixedDeltaTime;
+		var posTr = transform.position;
 		
-		_targetLocal += dt * noise * Random.insideUnitCircle;
-		_targetLocal = radius * _targetLocal.normalized;
-		
-		var targetWorld = transform.TransformPoint(_targetLocal + distance * Vector2.up);
-		var force = (Vector2)(targetWorld - transform.position);
-		rigidbody.AddForce(power * force);
+		if (!watertank.bounds.Contains(posTr)) {
+			var wallPosLocal = transform.InverseTransformPoint(watertank.ClosestPointOnBounds(posTr));
+			_targetLocal += dt* wallWeight * (Vector2)wallPosLocal;
+		}
 
+		_targetLocal += dt * wanderNoise * Random.insideUnitCircle;
+		_targetLocal = wanderRadius * _targetLocal.normalized;
+		
+		var targetWorld = transform.TransformPoint(_targetLocal + wanderDistance * Vector2.up);
+		var wanderForce = (Vector2)(targetWorld - posTr);
+		rigidbody.AddForce(wanderWeight * wanderForce);
+
+		ClampSpeed ();
+		
+		UpdateRotation ();
+	}
+
+	void ClampSpeed () {
 		if ((maxSpeed * maxSpeed) < rigidbody.velocity.sqrMagnitude) {
 			rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, maxSpeed);
 		} else if (rigidbody.velocity.sqrMagnitude < 1e-3f) {
 			rigidbody.velocity = minSpeed * (((Vector2)rigidbody.velocity).normalized + Random.insideUnitCircle);
 		}
-		
+	}
+
+	void UpdateRotation () {
 		if (1e-4f < rigidbody.velocity.sqrMagnitude) {
 			var forward = rigidbody.velocity.normalized;
+			forward.z = 0f;
 			rigidbody.MoveRotation(Quaternion.FromToRotation(Vector3.up, forward));
 		}
 	}
