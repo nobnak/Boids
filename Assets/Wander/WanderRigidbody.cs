12758ac16739e97126545366b66030c569d37734
nnakata <nakata@team-lab.com>
2013-10-28 14:07:07 +0900

    壁回避

--- Assets/Wander/WanderRigidbody.cs
+++ Assets/Wander/WanderRigidbody.cs
@@ -4,11 +4,15 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class WanderRigidbody : MonoBehaviour {
 	public Transform wanderTarget;
+	public Collider tank;
+	public Transform[] haptics;
 	
-	public float radius;
-	public float distance;
-	public float power;
-	public float noise;
+	public float wanderRadius;
+	public float wanderDistance;
+	public float wanderWeigt;
+	public float wanderNoise;
+	
+	public float wallAvoidWeight;
 	
 	public float maxSpeed;
 	public float minSpeed;
@@ -25,20 +29,36 @@ public class WanderRigidbody : MonoBehaviour {
 	void FixedUpdate () {
 		var dt = Time.fixedDeltaTime;
 		
-		_targetLocal += dt * noise * Random.insideUnitCircle;
-		_targetLocal = radius * _targetLocal.normalized;
+		_targetLocal += dt * wanderNoise * new Vector2(Random.value - 0.5f, Random.value - 0.5f); 
+		_targetLocal = wanderRadius * _targetLocal.normalized;
 		
-		var targetWorld = transform.TransformPoint(_targetLocal + distance * Vector2.up);
+		var targetWorld = transform.TransformPoint(_targetLocal + wanderDistance * Vector2.up);
 		wanderTarget.position = targetWorld;
-		var force = (Vector2)(targetWorld - transform.position);
-		rigidbody.AddForce(power * force);
+		var wanderForce = (Vector2)(targetWorld - transform.position);
+		rigidbody.AddForce(wanderWeigt * wanderForce);
+		
+		var wallAvoidForce = Vector2.zero;
+		foreach (var hap in haptics) {
+			var posHap = hap.position;
+			if (tank.bounds.Contains(posHap))
+				continue;
+			wallAvoidForce += (Vector2)(tank.ClosestPointOnBounds(posHap) - posHap);
+		}
+		rigidbody.AddForce(wallAvoidWeight * wallAvoidForce);
 
+		ClampSpeed();		
+		UpdateRotation();
+	}
+
+	void ClampSpeed () {
 		if ((maxSpeed * maxSpeed) < rigidbody.velocity.sqrMagnitude) {
 			rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, maxSpeed);
 		} else if (rigidbody.velocity.sqrMagnitude < 1e-3f) {
 			rigidbody.velocity = minSpeed * (((Vector2)rigidbody.velocity).normalized + Random.insideUnitCircle);
 		}
-		
+	}
+
+	void UpdateRotation () 	{
 		if (1e-4f < rigidbody.velocity.sqrMagnitude) {
 			var forward = rigidbody.velocity.normalized;
 			rigidbody.MoveRotation(Quaternion.FromToRotation(Vector3.up, forward));
